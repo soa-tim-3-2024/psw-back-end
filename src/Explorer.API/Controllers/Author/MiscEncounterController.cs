0cee28b1901c82a015136f06d49c8307c19b9d44
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2024-03-13 03:53:52 +0100

    api link established

--- src/Explorer.API/Controllers/Author/MiscEncounterController.cs
+++ src/Explorer.API/Controllers/Author/MiscEncounterController.cs
@@ -2,6 +2,9 @@
 using Explorer.Encounters.API.Public;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 
 namespace Explorer.API.Controllers.Author
 {
@@ -10,16 +13,39 @@ namespace Explorer.API.Controllers.Author
     public class MiscEncounterController : BaseApiController
     {
         private readonly IEncounterService _encounterService;
-        public MiscEncounterController(IEncounterService encounterService) {
+        private readonly IHttpClientFactory _factory;
+        public MiscEncounterController(IEncounterService encounterService, IHttpClientFactory factory)
+        {
             _encounterService = encounterService;
+            _factory = factory;
         }
 
 
         [HttpPost("createMisc")]
-        public ActionResult<MiscEncounterResponseDto> Create([FromBody] MiscEncounterCreateDto encounter)
+        public async Task<ActionResult<MiscEncounterResponseDto>> CreateSocialEncounter([FromBody] MiscEncounterResponseDto miscEncounter)
         {
-            var result = _encounterService.CreateMiscEncounter(encounter);
-            return CreateResponse(result);
+            var client = _factory.CreateClient();
+            var encounterResponse = await CreateMiscEncounterGo(client, miscEncounter);
+            return encounterResponse;
+        }
+
+        static async Task<MiscEncounterResponseDto> CreateMiscEncounterGo(HttpClient httpClient, MiscEncounterResponseDto encounter)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(encounter),
+                Encoding.UTF8,
+                "application/json");
+            Debug.WriteLine(jsonContent.ToString());
+            string jsonString = jsonContent.ReadAsStringAsync().Result;
+            Debug.WriteLine(jsonString);
+            Debug.WriteLine("!!!");
+            Debug.WriteLine("!!!");
+            using HttpResponseMessage response = await httpClient.PostAsync(
+                "http://localhost:8082/misc/encounters",
+                jsonContent);
+            Debug.WriteLine(jsonContent.ReadAsStringAsync().Result);
+            var encounterResponse = await response.Content.ReadFromJsonAsync<MiscEncounterResponseDto>();
+            return encounterResponse;
         }
 
     }
